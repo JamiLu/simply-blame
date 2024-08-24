02c50ab39b7fb2522165d8bf8ad6ad00324e9a93
Jun10ng <zeonll@outlook.com>
2024-08-24 18:29:29 +0800

    support custom dateFormate by adding momentjs

--- src/Date.ts
+++ src/Date.ts
@@ -1,25 +1,12 @@
 import * as vscode from "vscode";
 import Settings from "./Settings";
-
-const appendZeroToString = (num: number): string => {
-    return num < 10 ? `0${num}` : `${num}`;
-};
+import moment = require("moment");
 
 export const parseDate = (date: Date) => {
-    const dateFormat = Settings.getDateFormat();
-    if (dateFormat === 'system') {
-        return date.toLocaleDateString(vscode.env.language);
-    }
+  const dateFormat = Settings.getDateFormat();
+  if (dateFormat === "system") {
+    return date.toLocaleDateString(vscode.env.language);
+  }
 
-    const delimitter = dateFormat.match(/\-|\.|\//)?.join();
-    const startDate = dateFormat.indexOf('D') === 0;
-    const days = date.getDate();
-    const months = date.getMonth() + 1;
-    const year = date.getFullYear();
-
-    if (startDate) {
-        return [days, months, year].map(appendZeroToString).join(delimitter);
-    } else {
-        return [months, days, year].map(appendZeroToString).join(delimitter);
-    }
+  return moment(date).format(dateFormat);
 };
