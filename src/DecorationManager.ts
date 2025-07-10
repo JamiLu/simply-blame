862d34f6345acbcd0f42e9abc1f9cc4cb4d8f9a3
Jami Lu <lu.jami90gmail.com>
2025-07-10 14:50:57 +0300

    update readme & finetune code

--- src/DecorationManager.ts
+++ src/DecorationManager.ts
@@ -16,6 +16,7 @@ class DecorationManager {
     private defaultWidth = '70px';
 
     constructor(heatmapManager: HeatMapManager) {
+        this.refresh();
         this.heatMapManager = heatmapManager;
     }
 
